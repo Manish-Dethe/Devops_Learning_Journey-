ef0056054fef1af958f69a66fb8cc25b54b39b8a
Manish-Dethe <manishdethe4@gmail.com>
2026-05-02 16:44:20 +0530

    Project3_Log_Automation

--- 01_Linux/Project3_Log_Automation/analyze_logs.sh
+++ 01_Linux/Project3_Log_Automation/analyze_logs.sh
@@ -1,1 +1,14 @@
 #!/bin/bash
+
+echo "===== Log Analysis Report ====="
+
+error_count=$(grep -c "ERROR" app.log)
+warning_count=$(grep -c "WARNING" app.log)
+
+echo "Total Errors: $error_count"
+echo "Total Warnings: $warning_count"
+
+echo "------------------------------"
+echo "ERROR Logs:"
+
+grep "ERROR" app.log
